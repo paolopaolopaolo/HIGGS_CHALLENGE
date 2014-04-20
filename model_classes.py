8ca4e700d981876853a61d385300cd6e31a446ef
paolopaolopaolo <dpaolomercado@gmail.com>
2014-04-20 13:52:18 -0700

    added logistic regression, training model only

--- model_classes.py
+++ model_classes.py
@@ -1,4 +1,4 @@
-import numpy as np, sys, scipy as sp
+import numpy as np, sys, scipy as sp, csv,random
 from scipy import optimize
 
 class Logistic_Regression_Model():
@@ -240,9 +240,22 @@ class Logistic_Regression_Model():
 		else:
 			self.train(opt=None)
 
-	def test(self, m=None):
-		if m is None:
-			tries= self.m
+	def test(self, test_set_filename):
+		data=np.loadtxt(test_set_filename, delimiter=",")
+
+		m=data.shape[0]
+		tries=m
+		
+		try:
+			ycol=int(raw_input("What is the y-col for the test set?"))
+		
+		except TypeError:
+			print "Try again!"
+			self.test(test_set_filename)
+
+		y=data[:,ycol]
+		X=sp.delete(data,ycol,1)
+		X=np.hstack((np.ones((self.m,1)), X))
 
 		hx0y0=0
 		hx1y0=0
@@ -251,19 +264,19 @@ class Logistic_Regression_Model():
 		other=0
 
 		for i in range(0,tries):
-			if (self.logistic_hypothesis(self.X[i,:],self.theta) >=0.5 and self.y[i] ==1):
+			if (self.logistic_hypothesis(X[i,:],self.theta) >=0.5 and y[i] ==1):
 				hx1y1+=1
-			elif (self.logistic_hypothesis(self.X[i,:],self.theta) <0.5 and self.y[i] ==0):
+			elif (self.logistic_hypothesis(X[i,:],self.theta) <0.5 and y[i] ==0):
 				hx0y0+=1
-			elif (self.logistic_hypothesis(self.X[i,:],self.theta) >= 0.5 and self.y[i] ==0):
+			elif (self.logistic_hypothesis(X[i,:],self.theta) >= 0.5 and y[i] ==0):
 				hx1y0+=1
-			elif (self.logistic_hypothesis(self.X[i,:],self.theta) < 0.5 and self.y[i] ==1):
+			elif (self.logistic_hypothesis(X[i,:],self.theta) < 0.5 and y[i] ==1):
 				hx0y1+=1
 			else:
 				other+=1
 
 
-		print "Accuracy on training data:\n"
+		print "Accuracy on test data:\n"
 		print "hx\t0\t1"
 		print "y 0\t"+str(hx0y0)+"\t"+str(hx1y0)
 		print "  1\t"+str(hx0y1)+"\t"+str(hx1y1)
@@ -271,18 +284,278 @@ class Logistic_Regression_Model():
 		print "Accuracy: %02.2f" % float((float(hx1y1+hx0y0)/float(tries))*100.)
 
 
+class Neural_Network_Model():
+	"""
+
+	Neural network is an advanced supervised machine learning
+	classification method that utilizes a "hidden layer" to create
+	complex calculation models, enabling the generation of non-linear 
+	hypotheses. 
+
+	This model will use the sigmoidal logistic function as the 
+	activation function and comes with a choice of 1,2 or 3 
+	hidden layers of varying node number. The backpropagation
+	algorithm is used to train the neural network
+
+	"""
+
+	## Architecture ##
+	data_file=""
+	m=0
+	lamb=0
+	y_col=0
+	input_nodes=0
+	hidden_layer_num=0
+	hidden_layer_nodes=[]
+	output_nodes=0
+
+	## Parameters ##
+	init_nnparams=None
+	nnparams=None
+	Theta1_par=(0,0)
+	Theta2_par=(0,0)
+	Theta3_par=(0,0)
+	ThetaEnd_par=None
+
+	def mult_tuple(self,tuple):
+		res=1
+		for i in tuple:
+			res*=i
+		return res
+
+	def set_architecture(self):
+		try:
+			self.input_nodes=int(raw_input("How many features are in X?: "))
+			self.hidden_layer_num=int(raw_input("How many hidden layers will you use (1-3)?: "))
+
+			if self.hidden_layer_num < 1 or self.hidden_layer_num>3:
+				self.hidden_layer_num=1
+				print "Warning: Invalid value. 1 hidden layer selected."
+			
+			self.hidden_layer_nodes=[]
+			for i in range(0,self.hidden_layer_num):
+				add_nodes=int(raw_input("How many nodes will be added to hidden layer "+str(i+1)+"?: "))
+
+				self.hidden_layer_nodes.append(self.input_nodes+add_nodes)
+
+			self.output_nodes=int(raw_input("How many distinct classes are in the dataset?: "))
+
+		except (ValueError,TypeError):
+			print "Invalid values! Try again!"
+			self.set_architecture()
+
+	def randomize_nnparams(self):
+
+		eps=((6.0)**(0.5))/((float(self.input_nodes)+float(self.output_nodes))**(0.5))
+
+		self.Theta1_par=(self.hidden_layer_nodes[0], self.input_nodes+1)
+		self.ThetaEnd_par=(self.output_nodes, self.hidden_layer_nodes[-1]+1)
+
+		if self.hidden_layer_num>1:
+
+			self.Theta2_par=(self.hidden_layer_nodes[1],self.hidden_layer_nodes[0]+1)
+
+			if self.hidden_layer_num>2:
+
+				self.Theta3_par=(self.hidden_layer_nodes[2],self.hidden_layer_nodes[1]+1)
+				
+		self.init_nnparams=np.random.rand(self.mult_tuple(self.Theta1_par)+self.mult_tuple(self.ThetaEnd_par)+self.mult_tuple(self.Theta2_par)+self.mult_tuple(self.Theta3_par),)*(2*eps)-eps
+		self.nnparams=np.zeros_like(self.init_nnparams)
 
 
+	def __init__(self, filename, y_col, m=None, input_nodes=0, hidden_layer_num=0, hidden_layer_nodes=[], output_nodes=0):
+		"""filenames= the string path of the CSV data to be fed into the model
+		y_col= integer value of the column of the training instance (0 indexed)
+		hidden_layer_num= number of hidden layers (can be 1-3)
+		hidden_layer_nodes= list corresponding to the number of nodes in 
+		                    each hidden layer
+		output_nodes= number of output nodes"""
 
+		#checks number of rows in file
+		if m is None:
+			infile=open(filename,'rb')
+			for line in infile:
+				self.m+=1
+			infile.close()
+		else:
+			self.m=m
 
-
-
-
-
-	
-
-
-
+		if self.data_file!="":
+			self.data_file=""
 
 		
-	
+		self.data_file+=filename
+		self.y_col= y_col
+		self.input_nodes=input_nodes
+		self.hidden_layer_num=hidden_layer_num
+		self.hidden_layer_nodes=hidden_layer_nodes
+
+		if self.hidden_layer_num != len(hidden_layer_nodes):
+			raise IndexError
+		self.output_nodes=output_nodes
+		
+		if input_nodes==0 or hidden_layer_num==0 or len(hidden_layer_nodes)==0 or output_nodes==0:
+			self.set_architecture()
+
+		self.randomize_nnparams()
+			
+		
+	def review_architecture(self):
+		print "~~Architecture~~"
+		print "Number of Examples: "+str(self.m)
+		print "Input Nodes: "+str(self.input_nodes)
+		hid_layer_count=1
+		for num in self.hidden_layer_nodes:
+			print "Hidden Layer "+str(hid_layer_count)+": "+str(num)
+			hid_layer_count+=1
+		print "Output Nodes: "+str(self.output_nodes)
+
+	def __call__(self, filename):
+		pass
+
+	def train(self, opt=None,lamb=0):
+
+		self.lamb=lamb
+
+		def sigmoidGradient(z):
+			return (1.0/(1.0+np.e**-z))*(1.0-(1.0/1.0+np.e**-z))
+
+		def sigmoid(z):
+			return 1.0/(1.0+np.e**-z)
+
+		other_args=(self.data_file,self.y_col,self.m,self.lamb,self.Theta1_par,self.Theta2_par,self.Theta3_par,self.ThetaEnd_par,self.input_nodes,self.hidden_layer_num,self.hidden_layer_nodes,self.output_nodes)
+
+		def cost(nnparams,data_file,y_col,m,lamb,Theta1_par,Theta2_par,Theta3_par,ThetaEnd_par,input_nodes,hidden_layer_num, hidden_layer_nodes, output_nodes):
+
+			def mult_tuple(tupl):
+				res=1
+				for i in tupl:
+					res*=i
+				return res
+
+			with open(data_file,"rb") as f: # self.data_file
+				row_read=csv.reader(f,delimiter=",")
+
+				ymap=np.eye(output_nodes,dtype=float) # self.output_nodes
+				Theta1=nnparams[0:(mult_tuple(Theta1_par))].reshape(Theta1_par) # self.nnparams, self.Theta1_par
+				Theta2=0
+				Theta3=0
+				ThetaEnd=0
+
+				#Populate parameters with randomized values (randomized in randomize_nnparams method).
+
+				if hidden_layer_num==1: #self.hidden_layer_num
+					ThetaEnd=nnparams[(mult_tuple(Theta1_par)):(mult_tuple(Theta1_par)+mult_tuple(ThetaEnd_par))].reshape(ThetaEnd_par)
+					
+				elif hidden_layer_num==2:
+					Theta2=nnparams[(mult_tuple(Theta1_par)):(mult_tuple(Theta1_par)+mult_tuple(Theta2_par))].reshape(Theta2_par)
+					ThetaEnd=nnparams[(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)):(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)+mult_tuple(ThetaEnd_par)+1)].reshape(ThetaEnd_par)
+
+				else:
+					Theta2=nnparams[(mult_tuple(Theta1_par)):(mult_tuple(Theta1_par)+mult_tuple(Theta2_par))].reshape(Theta2_par)
+					Theta3=nnparams[(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)):(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)+mult_tuple(Theta3_par))].reshape(Theta3_par)
+					ThetaEnd=nnparams[(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)+mult_tuple(Theta3_par)):(mult_tuple(Theta1_par)+mult_tuple(Theta2_par)+mult_tuple(Theta3_par)+mult_tuple(ThetaEnd_par)+1)].reshape(ThetaEnd_par)
+					
+				J=0
+				grad=np.zeros_like(nnparams)
+
+				Theta1_grad=np.zeros_like(Theta1)
+				ThetaEnd_grad=np.zeros_like(ThetaEnd)
+				Theta2_grad=0
+				Theta3_grad=0
+
+				if hidden_layer_num > 1:
+					Theta2_grad=np.zeros_like(Theta2)
+
+					if hidden_layer_num == 3:
+						Theta3_grad=np.zeros_like(Theta3)
+
+				for i in range(0,m):
+					#Load in Row
+					ROW=row_read.next()
+					data=np.array(ROW, dtype=float)
+					y=ymap[:,int(data[y_col])].reshape((output_nodes,1)) # output_nodes x 1
+					X=np.hstack((1.0,sp.delete(data,y_col))) # 1 x (n+1)
+			
+					#Forward Propagation
+					z2=np.dot(Theta1,X.reshape((len(X),1)))  #(hidden_layer_node[0] x n+1) X (n+1 x 1) = hidden_layer_node[0] x 1		
+					a2=np.vstack((1.0, sigmoidGradient(z2))) # (hidden_layer_node [0] + 1 )x 1
+					aEnd=np.zeros((output_nodes,1))
+					if hidden_layer_num==1:
+						z3=np.dot(ThetaEnd,a2) # (output_node x (hidden_layer_node [0] + 1 )) x ((hidden_layer_node [0] + 1) x 1)
+						a3=sigmoid(z3)
+						aEnd=a3
+
+					elif hidden_layer_num==2:
+						z3=np.dot(Theta2,a2)
+						a3=np.vstack((1.0,sigmoid(z3)))
+						z4=np.dot(ThetaEnd,a3)
+						a4=sigmoid(z4)
+						aEnd=a4
+
+					else:
+						z3=np.dot(Theta2,a2)
+						a3=np.vstack((1.0,sigmoid(z3)))
+						z4=np.dot(Theta3,a3)
+						a4=np.vstack((1.0,sigmoid(z4)))
+						z5=np.dot(ThetaEnd,a4)
+						a5=sigmoid(z5)
+						aEnd=a5
+
+					#Compute J and add to current J
+					J=J-sum(y*np.log(aEnd)+(1.0-y)*np.log(1.0-aEnd))[0]
+					
+					#Backward Propagation
+					deltEnd=np.zeros(aEnd.shape)
+					deltEnd=aEnd-y
+
+					if hidden_layer_num==1:
+
+						delt2=np.dot(ThetaEnd.transpose(), deltEnd) * np.vstack((1.0,sigmoidGradient(z2)))
+						ThetaEnd_grad= ThetaEnd_grad +np.dot(deltEnd,a2.transpose())	
+						Theta1_grad= Theta1_grad + np.dot(delt2[1:],X.reshape((1,len(X))))
+
+					elif hidden_layer_num==2:
+
+						delt3=np.dot(ThetaEnd.transpose(), deltEnd) * np.vstack((1.0,sigmoidGradient(z3)))
+						delt2=np.dot(Theta2.transpose(), delt3[1:,:]) * np.vstack((1.0,sigmoidGradient(z2)))
+						ThetaEnd_grad=ThetaEnd_grad+np.dot(deltEnd,a3.transpose())
+						Theta2_grad=Theta2_grad+np.dot(delt3[1:,:],a2.transpose())					
+						Theta1_grad=Theta1_grad+np.dot(delt2[1:,:],X.reshape((1,len(X))))
+
+					else:
+
+						delt4=np.dot(ThetaEnd.transpose(), deltEnd) * np.vstack((1.0,sigmoidGradient(z4)))
+						delt3=np.dot(Theta3.transpose(), delt4[1:,:]) * np.vstack((1.0,sigmoidGradient(z3)))
+						delt2=np.dot(Theta2.transpose(), delt3[1:,:]) * np.vstack((1.0,sigmoidGradient(z2)))
+						ThetaEnd_grad=ThetaEnd_grad+np.dot(deltEnd, a4.transpose())
+						Theta3_grad=Theta3_grad+np.dot(delt4[1:,:],a3.transpose())
+						Theta2_grad=Theta2_grad+np.dot(delt3[1:,:],a2.transpose())
+						Theta1_grad=Theta1_grad+np.dot(delt2[1:,:],X.reshape((1,len(X))))
+						
+					
+					print "\b"*50+str(i)+"/"+str(m)+" rows",
+
+				J=(1.0/m)*J
+				J=J+(lamb/(2*m))*sum(nnparams**2)
+
+				Theta1_grad=(1.0/m)*Theta1_grad
+				Theta2_grad=(1.0/m)*Theta2_grad
+				Theta3_grad=(1.0/m)*Theta3_grad
+				ThetaEnd_grad=(1.0/m)*ThetaEnd_grad
+
+				if hidden_layer_num==1:
+					grad=np.hstack((Theta1_grad.reshape((mult_tuple(Theta1_grad.shape),)),ThetaEnd_grad.reshape((mult_tuple(ThetaEnd_grad.shape),))))
+
+				elif hidden_layer_num==2:
+					grad=np.hstack((Theta1_grad.reshape((mult_tuple(Theta1_grad.shape),)),ThetaEnd_grad.reshape((mult_tuple(ThetaEnd_grad.shape),)),Theta2_grad.reshape((mult_tuple(Theta2_grad.shape),))))
+				
+				else:
+					grad=np.hstack((Theta1_grad.reshape((mult_tuple(Theta1_grad.shape),)),ThetaEnd_grad.reshape((mult_tuple(ThetaEnd_grad.shape),)),Theta2_grad.reshape((mult_tuple(Theta2_grad.shape),)),Theta3_grad.reshape((mult_tuple(Theta3_grad.shape),))))
+
+				return (J,grad)
+
+		#def gradient(nnparams,data_file,y_col,m,lamb,Theta1_par,Theta2_par,Theta3_par,ThetaEnd_par,input_nodes,hidden_layer_num, hidden_layer_nodes, output_nodes):
+
+		self.nnparams=sp.optimize.minimize(fun=cost, x0=self.init_nnparams, jac=True, method=opt,options={'maxiter':75,'disp':True}, args=other_args)
+
